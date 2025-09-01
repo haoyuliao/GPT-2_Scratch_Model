13b607970098cf0f00a6178a8ac7cdda2d04d4fe
haoyuliao <haoyuliao@ufl.edu>
2025-08-31 22:07:49 -0400

    Update_modules

---
+++ TransformerBlock.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+from MultiHeadAttention import MultiHeadAttention
+from FeedForward import FeedForward
+from LayerNorm import LayerNorm
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate_attn"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate_shortcut"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        #print(x)
+        x = self.drop_shortcut(x)
+        #print(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
